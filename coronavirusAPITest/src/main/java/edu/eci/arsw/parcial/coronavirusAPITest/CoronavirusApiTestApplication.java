23e84f930eeaa17e7a636656f08e4b82e8fa9bb4
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-03-25 07:23:20 -0500

    prueba Heroku

---
+++ coronavirusAPITest/src/main/java/edu/eci/arsw/parcial/coronavirusAPITest/CoronavirusApiTestApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.parcial.coronavirusAPITest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusApiTestApplication.class, args);
+	}
+
+}
